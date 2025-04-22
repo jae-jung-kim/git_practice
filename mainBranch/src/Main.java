b7fae05539e8ef67198da17728c796f23e008870
김재중 <podosi158@gmail.com>
2025-04-22 22:31:11 +0900

    feat sub branch에 파일수정해서 올리기

--- mainBranch/src/Main.java
+++ mainBranch/src/Main.java
@@ -3,5 +3,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.printf("Main Branch!");
         System.out.println("Sub Branch!");
+        System.out.println("sub_of_sub1!");
     }
 }
